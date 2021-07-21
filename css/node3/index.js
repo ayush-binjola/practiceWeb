47a2f616ed0d8cbeb011caa1e905851968f55acb
ayush <1998ayushb@gmail.com>
2021-07-21 13:00:13 +0530

    first commit

---
+++ css/node3/index.js
@@ -0,0 +1,13 @@
+const http=require('http');
+const fs =require('fs');
+const hostname='127.0.0.1';
+const port=3000;
+
+const server=http.createServer((req,res)=>{
+    res.statusCode=200;
+res.setHeader({'Content-type':'text/Html'})
+res.end("hello");
+})
+server.listen(port,hostname,()=>{
+    console.log('server running at http://${hostname} : ${port} ');
+});
